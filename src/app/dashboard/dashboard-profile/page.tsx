a03f20e6b1ccf0d4ab6396098f5513ec519b8a0f
Diego Galdames <galdamesdiego98@gmail.com>
2025-01-11 17:48:43 -0300

    Componente de user-profile hecho con props

--- src/app/dashboard/dashboard-profile/page.tsx
+++ src/app/dashboard/dashboard-profile/page.tsx
@@ -3,7 +3,17 @@ import UserProfile from "@/components/dashboard-components/user-profile"
 export default function ProfilePage(){
     return(
         <div>
-            <UserProfile></UserProfile>
+            <UserProfile 
+                userName={"Felipe Galvez"}
+                userCell="933944228"
+                userMail={"dgaldames@pregrado.ubo.cl"}
+                userService={"Jardineria General"}
+                userDescription="Había una vez una persona que era toilet y luego llegó otra que era toilet..."
+                userSchedule="Lunes a Viernes de 9:00 a 18:00"
+                userPrice="$10000"
+                userExperience="10 años en el rubro de la jardineria"
+                >
+            </UserProfile>
         </div>
     )
 }
