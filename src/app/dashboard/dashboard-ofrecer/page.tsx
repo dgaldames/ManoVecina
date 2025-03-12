cc30277d88427cde82df58de96f07df0cb081a59
Diego Galdames <galdamesdiego98@gmail.com>
2025-03-12 12:41:29 -0300

    Los campos se deshabilitan si tienen datos ingresados y restriccion de solo numeros al telefono.

--- src/app/dashboard/dashboard-ofrecer/page.tsx
+++ src/app/dashboard/dashboard-ofrecer/page.tsx
@@ -65,6 +65,7 @@ export default function OfrecerPage(){
         }));
     };
 
+
     const handleSubmit = async (e: React.FormEvent) => { //Los "e", hacen referencia evento que general el usuario.
         e.preventDefault();
         setLoading(true);
@@ -109,11 +110,12 @@ export default function OfrecerPage(){
                     <div className="grid gap-6 mb-6 md:grid-cols-2">
                         <div>
                             <label htmlFor="nombre" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre (Este nombre se utilizará para dar a conocer sus servicios, puede ser un apodo)</label>
-                            <input value={formData.nombre} onChange={handleChange} type="text" id="nombre" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Titor" required />
+                            <input disabled={!!nombre} value={formData.nombre} onChange={handleChange} type="text" id="nombre" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John Titor" required />
+                            {/* Se deshabilita el campo si ya existe un nombre */}
                         </div>
                         <div>
                             <label htmlFor="nom_serv" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre del Servicio</label>
-                            <input value={formData.nom_serv} onChange={handleChange} type="text" id="nom_serv" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Jardineria General" required />
+                            <input disabled={!!nom_serv} value={formData.nom_serv} onChange={handleChange} type="text" id="nom_serv" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Jardineria General" required />
                             <p className = {`text-sm mt-1 ml-1 ${formData.nom_serv.length === 50 ? "text-vecino"
                                                 : formData.nom_serv.length > 50
                                                 ? "text-red-500"
@@ -125,7 +127,7 @@ export default function OfrecerPage(){
                         </div>
                         <div>
                             <label htmlFor="telefono" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Teléfono</label>
-                            <input value={formData.telefono} onChange={handleChange} type="tel" id="telefono" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="912345678" required />
+                            <input disabled={!!telefono} value={formData.telefono} onChange={handleChange} type="tel" pattern="[0-9]" id="telefono" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="912345678" required />
                             <p
                                 className={`mt-1 text-sm ml-1 ${
                                     formData.telefono.length === 9 ? "text-vecino"
@@ -139,7 +141,7 @@ export default function OfrecerPage(){
                         </div>
                         <div>
                             <label htmlFor="disponibilidad" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Disponibilidad Horaria</label>
-                            <input value={formData.disponibilidad} onChange={handleChange} type="text" id="disponibilidad" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Lunes a Viernes de 10:00 a 15:00" required />
+                            <input disabled={!!disponibilidad} value={formData.disponibilidad} onChange={handleChange} type="text" id="disponibilidad" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Lunes a Viernes de 10:00 a 15:00" required />
                             <p className = {`text-sm mt-1 ml-1 ${formData.disponibilidad.length === 100 ? "text-vecino"
                                                 : formData.disponibilidad.length > 100
                                                 ? "text-red-500"
@@ -152,7 +154,7 @@ export default function OfrecerPage(){
 
                         <div>
                             <label htmlFor="tarifa" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tarifa por Servicio</label>
-                            <input value={formData.tarifa} onChange={handleChange} type="text" id="tarifa" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="$10.000 - $15.000" required />
+                            <input disabled={!!tarifa} value={formData.tarifa} onChange={handleChange} type="text" id="tarifa" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="$10.000 - $15.000" required />
                             <p className = {`text-sm mt-1 ml-1 ${formData.tarifa.length === 75 ? "text-vecino"
                                                 : formData.tarifa.length > 75
                                                 ? "text-red-500"
@@ -165,7 +167,7 @@ export default function OfrecerPage(){
                     </div>
                     <div className="mb-6">
                         <label htmlFor="descripcion" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descripción su Servicio</label>
-                        <input value={formData.descripcion} onChange={handleChange} type="text" id="descripcion" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Me especializo en la poda de arbustos y tala de arboles..." required />
+                        <input disabled={!!descripcion} value={formData.descripcion} onChange={handleChange} type="text" id="descripcion" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Me especializo en la poda de arbustos y tala de arboles..." required />
                         <p className = {`text-sm mt-1 ml-1 ${formData.descripcion.length === 250 ? "text-vecino"
                                                 : formData.descripcion.length > 250
                                                 ? "text-red-500"
@@ -178,7 +180,7 @@ export default function OfrecerPage(){
                     <p></p>
                     <div className="mb-6">
                         <label htmlFor="experiencia" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Experiencia de su Servicio (Opcional)</label>
-                        <input value={formData.experiencia} onChange={handleChange} type="text" id="experiencia" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cuento con mas de 10 anhos en el rubro..." required />
+                        <input disabled={!!experiencia} value={formData.experiencia} onChange={handleChange} type="text" id="experiencia" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cuento con mas de 10 anhos en el rubro..." required />
                         <p className = {`text-sm mt-1 ml-1 ${formData.experiencia.length === 250 ? "text-vecino"
                                                 : formData.experiencia.length > 250
                                                 ? "text-red-500"
@@ -205,7 +207,7 @@ export default function OfrecerPage(){
                         {/* TODO HACER QUE AL PRESIONAR CAMBIE EL COLOR CUANDO EL loading ES true */}
                         <button type="submit" className="text-white bg-vecino rounded-lg hover:bg-orange-700 focus:ring-2  dark:focus:ring-white focus:ring-darkbg focus:outline-none text-lg w-full lg:w-auto px-5 py-2.5 text-center transform hover:scale-105 hover:ease-out transition duration-300"
                         disabled={loading}
-                        >{loading ? "Publicando sus Servicios..." : "Publicar mis Servicios"}</button>
+                        >{loading ? "Publicando sus Servicios..." : "Publicar mi Servicio"}</button>
                     </div>
                 </form>
             </div>
