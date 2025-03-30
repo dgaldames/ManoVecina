e9b7ac5af90afde08fc1d1f3c73f3410e42d28fc
Diego Galdames <galdamesdiego98@gmail.com>
2025-03-30 12:39:03 -0300

    Ultimos cambios. Se ve en el dashboard la card de los servicios, pero solo muestra el de la sesion iniciada.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -17,16 +17,17 @@ export default async function Page() {
             {services.map((service) => (
                 <UserCard
                     key={service.id}
+                    userId={service.id}
                     userName={service.nombre}
                     userService={service.nom_serv}
                     userDetails={service.descripcion}
                 />
             ))}
-            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            {/* <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                 <UserCard userName={"Diego Galdames"} userService={"Informatico"} userDetails="Experto en desarrollo web y gestion de proyectos"></UserCard>
                 <UserCard userName={"Diego Galdames"} userService={"Informatico"} userDetails="Experto en desarrollo web y gestion de proyectos"></UserCard>
                 <UserCard userName={"Diego Galdames"} userService={"Informatico"} userDetails="Experto en desarrollo web y gestion de proyectos"></UserCard>
-            </div>
+            </div> */}
         </div>
     )
 }
--- src/components/dashboard-components/user-card.tsx
+++ src/components/dashboard-components/user-card.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 
-export default function UserCard ({ userName, userService, userDetails }:{userName: string, userService: string, userDetails: string}) {
+export default function UserCard ({userId, userName, userService, userDetails }:{userId: string, userName: string, userService: string, userDetails: string}) {
     return(
         <div className="w-full max-w-lg pt-10 bg-white border border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-900 transition-all duration-200 ease-in-out">
             <div className="flex flex-col items-center pb-8">
@@ -15,7 +15,7 @@ export default function UserCard ({ userName, userService, userDetails }:{userNa
                 <p className="text-sm text-center text-gray-500 dark:text-gray-400">{userDetails}</p>
 
                 <div className="flex mt-4 md:mt-6">
-                    <Link href={`/dashboard/dashboard-profile`} className="transform hover:scale-105 hover:ease-out transition duration-300 inline-flex items-center px-4 py-2 text-lg font-medium text-center text-white bg-vecino rounded-lg hover:bg-orange-600 focus:ring-2  dark:focus:ring-white focus:ring-darkbg focus:outline-none">Ver Perfil</Link>
+                    <Link href={`/dashboard/dashboard-profile/${userId}`} className="transform hover:scale-105 hover:ease-out transition duration-300 inline-flex items-center px-4 py-2 text-lg font-medium text-center text-white bg-vecino rounded-lg hover:bg-orange-600 focus:ring-2  dark:focus:ring-white focus:ring-darkbg focus:outline-none">Ver Perfil</Link>
                 </div>
             </div>
         </div>
