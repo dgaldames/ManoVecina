d15a51e1c7038c1bead7cd08a12eaeae39e72891
Diego Galdames <galdamesdiego98@gmail.com>
2025-04-02 11:49:53 -0300

    Quitadas las props estaticas que se le pasaban al perfil del usuario. Ya que se utiliza el contexto de la seison.

--- src/app/dashboard/dashboard-my-profile/page.tsx
+++ src/app/dashboard/dashboard-my-profile/page.tsx
@@ -4,13 +4,6 @@ export default function MyProfile(){
     return(
         <div>
             <MyProfilePage
-                userName={"Diego Galdames"}
-                userCell="933944228"
-                userService={"Tala de Arboles General"}
-                userDescription="Había una vez una persona que era Skibidi y luego llegó otra que era toilet... Había una vez una persona que era toilet y luego llegó otra que era toilet... Había una vez una persona que era toilet y luego llegó otra que era toilet... Había una vez una persona que era toilet y luego llegó otra que"
-                userSchedule="Lunes a Viernes de 9:00 a 18:00"
-                userPrice="$10000"
-                userExperience="10 años en el rubro de la tala de arboles"
             ></MyProfilePage>
         </div>
     )
