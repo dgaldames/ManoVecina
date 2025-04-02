36ceb9032643923d4f9dab818c37b9508e9be392
Diego Galdames <galdamesdiego98@gmail.com>
2025-04-02 14:59:05 -0300

    Ahora si hay servicio registrado saluda al nombre del usuario y si no al correo

--- src/components/saludo-user.tsx
+++ src/components/saludo-user.tsx
@@ -4,29 +4,39 @@ import React, { useEffect, useState } from "react";
 
 const UserGreetText = () => {
     const [user, setUser] = useState<{ id: string; email: string } | null>(null);
+    const [serviceName, setServiceName] = useState<string | null>(null);
     const supabase = createClient();
     useEffect(() => {
         const fetchUser = async () => {
-        const {
-            data: { user },
-        } = await supabase.auth.getUser();
-        setUser(user ? { id: user.id, email: user.email ?? "" } : null);
+            const {
+                data: { user },
+            } = await supabase.auth.getUser();
+            
+            if(user){
+                setUser({ id: user.id, email: user.email ?? "" });
+
+                const { data: serviceData, error } = await supabase
+                    .from("servicios_persona")
+                    .select("nombre")
+                    .eq("user_id", user.id) //Puede dar un error
+                    .single();
+
+                if(!error && serviceData && serviceData.nombre){
+                    setServiceName(serviceData.nombre);
+                }
+            }
         };
         fetchUser();
     }, []);
+
         if (user !== null) {
-        return (
-            <p>Hola! &nbsp;
-                <code>{user.email ?? "user"}</code>
-            </p>
-        );
+            return (
+                <p>Hola! &nbsp;
+                    <code>{serviceName ?? user.email}</code>
+                </p>
+            );
         }
-/*     return (
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        Get started editing&nbsp;
-        <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-    ); */
+    return null;
 };
 
 export default UserGreetText;
