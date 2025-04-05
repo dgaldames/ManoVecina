5c3a2e0ca389bf87707bd0305a65529067fb205e
Diego Galdames <galdamesdiego98@gmail.com>
2025-04-05 14:10:40 -0300

    pattern de telefono y prop

--- src/app/dashboard/dashboard-edit-profile/page.tsx
+++ src/app/dashboard/dashboard-edit-profile/page.tsx
@@ -155,7 +155,7 @@ const handleSubmit = async (e: React.FormEvent) => { //Los "e", hacen referencia
                         </div>
                         <div>
                             <label htmlFor="telefono" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Teléfono</label>
-                            <input value={formData.telefono} onChange={handleChange} type="tel" minLength={9} maxLength={9} pattern="[0-1-2-3-4-5-6-7-8-9]" id="telefono" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="912345678" required />
+                            <input value={formData.telefono} onChange={handleChange} type="tel" minLength={9} maxLength={9} pattern="[0-9]+" id="telefono" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="912345678" required />
                             <p
                                 className={`mt-1 text-sm ml-1 ${
                                     formData.telefono.length === 9 ? "text-vecino"
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -24,6 +24,7 @@ export default function Page() {
                     userName={service.nombre}
                     userService={service.nom_serv}
                     userDetails={service.descripcion}
+                    userImage={service.foto ? service.foto : "/dashboard-imgs/kurisu.png"}
                 />
             ))}
         </div>
