586336ed0f2f2b99e4caf9a332410ab5f2dd6e79
Diego Galdames <galdamesdiego98@gmail.com>
2025-04-05 14:11:28 -0300

    Esto no se si funciona pero lo dejare asi por las moscas

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ynhbwjmlrbicqklevxrn.supabase.co',
+        port: '',
+      }
+    ]
+  }
 };
+
+/* module.exports = {
+  images: {
+      domains: ['ynhbwjmlrbicqklevxrn.supabase.co'],
+  },
+  // ...otros ajustes...
+}; */
+
+
 
 export default nextConfig;
