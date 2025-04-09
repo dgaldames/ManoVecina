8a9079d09bdb311d34fa498613d1ccad4e923b3b
Diego Galdames <galdamesdiego98@gmail.com>
2025-04-09 16:51:03 -0400

    los iconos estaban bug dhgkjdg

--- public/ladingns-icons/funcionalidades/ofrecer-contratar-svg.tsx
+++ public/ladingns-icons/funcionalidades/ofrecer-contratar-svg.tsx
@@ -1,6 +1,6 @@
 export default function SvgOfrecerContratar(){
     return(
-        <svg className="w-14 h-14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg className="w-16 h-16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M21 7.5L8 7.5M21 7.5L16.6667 3M21 7.5L16.6667 12" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             <path d="M4 16.5L17 16.5M4 16.5L8.33333 21M4 16.5L8.33333 12" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
--- src/app/landing/landing-structure/target.tsx
+++ src/app/landing/landing-structure/target.tsx
@@ -10,7 +10,7 @@ export default function Objetivo(){
                         Historia
                     </h1>
                     <p className="text-xl leading-relaxed text-gray-800">
-                    <strong>ManoVecina nació como una idea para mi tesis universitaria</strong>, pero tras ser rechazada, decidí retomarla ya titulado como ingeniero en informática por la Universidad Bernardo O&apos;Higgins. Durante un periodo sin trabajo, con tiempo libre y ganas de crecer, opté por desarrollar esta plataforma con dos fines: ayudar a la comunidad y seguir aprendiendo como profesional. <strong>ManoVecina es, y seguirá siendo, un servicio gratuito</strong> que busca conectar a vecinos con personas que ofrecen ayuda en tareas cotidianas. Si alguna vez te es útil y deseas apoyar el proyecto, <strong>tendrás la opción totalmente voluntaria de hacer una donación monetaria</strong>.
+                    <strong>ManoVecina nació como una idea para mi tesis universitaria</strong>, pero tras ser rechazada, decidí retomarla ya titulado como ingeniero en informática por la Universidad Bernardo O&apos;Higgins. Durante un periodo sin trabajo, con tiempo libre y ganas de crecer, opté por desarrollar esta plataforma con dos fines: ayudar a la comunidad y seguir aprendiendo. <strong>ManoVecina es, y seguirá siendo, un servicio gratuito</strong> que busca conectar a vecinos con personas que ofrecen ayuda en tareas cotidianas. Si alguna vez te es útil y deseas apoyar el proyecto, <strong>tendrás la opción totalmente voluntaria de hacer una donación monetaria</strong>.
                     <span className="block mt-2 text-vecino font-semibold">
                         Donación voluntaria a Cuenta RUT: 21.088.563-3
                     </span>
